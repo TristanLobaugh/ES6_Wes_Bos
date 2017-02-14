919edcf47364e45d6759436e78da993931e21736
TristanLobaugh <ttrader_atl@yahoo.com>
2017-02-13 20:48:35 -0500

    commit vids 48-50 babel and polyfill

---
+++ 14 - ES6 Tooling/babel-MINE/app-compiled.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
+
+var age = 100;
+var people = ['Wes', 'Kait'];
+
+var fullName = people.map(function (name) {
+	return name + ' Bos';
+});
+
+console.log(age, fullName);
+
+var _iteratorNormalCompletion = true;
+var _didIteratorError = false;
+var _iteratorError = undefined;
+
+try {
+	for (var _iterator = people[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+		var person = _step.value;
+
+		console.log(person);
+	}
+} catch (err) {
+	_didIteratorError = true;
+	_iteratorError = err;
+} finally {
+	try {
+		if (!_iteratorNormalCompletion && _iterator.return) {
+			_iterator.return();
+		}
+	} finally {
+		if (_didIteratorError) {
+			throw _iteratorError;
+		}
+	}
+}
+
+var _x$y$a$b = { x: 1, y: 2, a: 3, b: 4 },
+    x = _x$y$a$b.x,
+    y = _x$y$a$b.y,
+    z = _objectWithoutProperties(_x$y$a$b, ['x', 'y']);
+
+console.log(x); // 1
+console.log(y); // 2
+console.log(z); // { a: 3, b: 4 }
+
+var n = _extends({ x: x, y: y }, z);
+console.log(n); // { x: 1, y: 2, a: 3, b: 4 }
--- 14 - ES6 Tooling/babel-MINE/app.js
+++ 14 - ES6 Tooling/babel-MINE/app.js
@@ -2,4 +2,18 @@
 const age = 100;
 const people = ['Wes', 'Kait'];
 
-console.log(age, people);
+const fullName = people.map(name => `${name} Bos`);
+
+console.log(age, fullName);
+
+for(const person of people) {
+	console.log(person);
+}
+
+let { x, y, ...z } = { x: 1, y: 2, a: 3, b: 4 };
+console.log(x); // 1
+console.log(y); // 2
+console.log(z); // { a: 3, b: 4 }
+
+let n = { x, y, ...z };
+console.log(n); // { x: 1, y: 2, a: 3, b: 4 }
